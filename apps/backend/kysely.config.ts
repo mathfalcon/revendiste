754f4a6aa56614e3c6b67905746839c7305f47cb
Mathias Falcon <mathfalcondj@gmail.com>
2025-12-14 23:38:50 -0300

    fix: migration run code

--- apps/backend/kysely.config.ts
+++ apps/backend/kysely.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "kysely-ctl";
-import { db } from "./src/db";
+import {defineConfig} from 'kysely-ctl';
+import {db} from './src/db';
+import {NODE_ENV} from './src/config/env';
 
 export default defineConfig({
   destroyOnExit: true,
   kysely: db,
   migrations: {
-    migrationFolder: "./src/db/migrations",
+    migrationFolder:
+      NODE_ENV !== 'local' ? './dist/src/db/migrations' : './src/db/migrations',
+    allowJS: NODE_ENV !== 'local',
   },
 });
